43d4d94582dc0b20e1ec074b5e45f67d41df4381
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2025-08-17 18:58:58 -0300

    New endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 const app = express();
 
 app.use(cors({optionsSuccessStatus: 200}));
